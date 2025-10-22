6be78a12849c39204dfa5e62fa7a4412d3110f6c
Marc Kurz <marc.kurz@fh-ooe.at>
2025-10-22 12:51:14 +0200

    changed unit test

--- src/test/java/com/example/hello/AppTest.java
+++ src/test/java/com/example/hello/AppTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test void greet_default_whenNameBlank() {
-        assertEquals("Hello, world!!", App.greet(""));
+        assertEquals("Hello, world!", App.greet(""));
     }
     @Test void greet_personalized() {
         assertEquals("Hello, Alice!", App.greet("Alice"));
