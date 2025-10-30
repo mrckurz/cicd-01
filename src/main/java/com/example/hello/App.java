67467a98bedf80e11f762a3d8667916ceaf96545
Marc Kurz <marc.kurz@fh-ooe.at>
2025-10-30 10:35:32 +0100

    trigger pipeline

--- src/main/java/com/example/hello/App.java
+++ src/main/java/com/example/hello/App.java
@@ -3,7 +3,7 @@ package com.example.hello;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Java CI!!");
+        System.out.println("Hello, Java CI!");
     }
     public static String greet(String name) {
         if (name == null || name.isBlank()) return "Hello, world!";
